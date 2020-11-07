1f642f20fe86bf496fbe5cc2237a54aef558f3c1
renatofrancisco.s <renatofrancisco.s@gmail.com>
2020-11-07 13:20:40 -0300

    1.5 Entendendo a Validação

--- python-brasilidades/main.py
+++ python-brasilidades/main.py
@@ -1,12 +1,12 @@
 from validate_docbr import CPF
 from Cpf import Cpf
 
-# cpf = '15616987913'
-
-# objeto_cpf = Cpf(cpf)
+#  cpf = '15616987913'
+objeto_cpf = Cpf('15316264754')
+print(objeto_cpf)
 
 # print(objeto_cpf)
 
-cpf = CPF()
-print(cpf.validate('012.345.678-90'))
-print(cpf.validate('11111111111'))
+# cpf = CPF()
+# print(cpf.validate('012.345.678-90'))
+# print(cpf.validate('11111111111'))
