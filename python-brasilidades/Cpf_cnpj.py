44f17207b5e3dd93d75ed7135930444f5bb8b39a
renatofrancisco.s <renatofrancisco.s@gmail.com>
2020-11-07 15:52:13 -0300

    2.2 Mascara para CNPJ

--- python-brasilidades/Cpf_cnpj.py
+++ python-brasilidades/Cpf_cnpj.py
@@ -20,22 +20,26 @@ class CpfCnpj:
             raise ValueError('Documento inválido!')
 
     def __str__(self):
-        return self.format_cpf()
+        if self.tipo_documento == 'cpf':
+            return self.format_cpf()
+        return self.format_cnpj()
 
     def cpf_eh_valido(self, cpf):
         if len(cpf) == 11:
             validador = CPF()
             return validador.validate(cpf)
-
         raise ValueError('Quantidade de dígitos inválida')
     
     def cnpj_eh_valido(self, cnpj):
         if len(cnpj) == 14:
             validador = CNPJ()
             return validador.validate(cnpj)
-
         raise ValueError('Quantidade de dígitos inválidas')
 
     def format_cpf(self):
         marcara = CPF()
         return marcara.mask(self.cpf)
+
+    def format_cnpj(self):
+        mascara = CNPJ()
+        return mascara.mask(self.cnpj)
--- python-brasilidades/main.py
+++ python-brasilidades/main.py
@@ -1,6 +1,7 @@
 from Cpf_cnpj import CpfCnpj
 
 exemplo_cnpj = '35379838000112'
-documento = CpfCnpj(exemplo_cnpj, 'cnpj')
+exemplo_cpf = '32007832062'
 
-# print(documento)
+print(CpfCnpj(exemplo_cnpj, 'cnpj'))
+print(CpfCnpj(exemplo_cpf, 'cpf'))
