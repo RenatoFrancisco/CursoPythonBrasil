92bfccbf9b312591ee313437ec43f842b32b18e7
renatofrancisco.s <renatofrancisco.s@gmail.com>
2020-11-07 13:08:27 -0300

    1.3 Pacotes Python

--- python-brasilidades/main.py
+++ python-brasilidades/main.py
@@ -1,7 +1,12 @@
+from validate_docbr import CPF
 from Cpf import Cpf
 
-cpf = '15616987913'
+# cpf = '15616987913'
 
-objeto_cpf = Cpf(cpf)
+# objeto_cpf = Cpf(cpf)
 
-print(objeto_cpf)
+# print(objeto_cpf)
+
+cpf = CPF()
+print(cpf.validate('012.345.678-90'))
+print(cpf.validate('11111111111'))
